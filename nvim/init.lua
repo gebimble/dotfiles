a6075a9b85b8de903b06c9d41286386733bd23d8
Joe Beaver <joseph.beaver@gmail.com>
2023-08-14 22:09:23 +0100

    Nice times.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,4 @@
 require("config.lazy")
 require("config.colorscheme")
 require("config.lsp")
+require("config.toggleterm")
--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,4 +1,4 @@
 -- require('monokai').setup {}
 require("monokai").setup({ palette = require("monokai").pro })
--- require('monokai').setup { palette = require('monokai').soda }
--- require('monokai').setup { palette = require('monokai').ristretto }
+-- require("monokai").setup({ palette = require("monokai").soda })
+-- require("monokai").setup({ palette = require("monokai").ristretto })
